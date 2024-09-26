94cadcba88300a6dd9cb461a1ca78c8dc5310e06
lak-sh-man <lakshman1122000@gmail.com>
2024-09-26 15:08:26 +0530

    commited:\

---
+++ 02-Flask-Smorest/02-Api-Improvements/01-Smorest-Abort/app.py
@@ -0,0 +1,63 @@
+import uuid
+from flask import Flask, request
+
+from db import stores, items
+
+
+app = Flask(__name__)
+
+
+@app.get("/store")
+def get_stores():
+    return {"stores": list(stores.values())}
+
+
+@app.post("/store")
+def create_store():
+    store_data = request.get_json()
+    store_id = uuid.uuid4().hex
+    store = {**store_data, "id": store_id}
+    stores[store_id] = store
+
+    return store
+
+
+@app.post("/item")
+def create_item():
+    item_data = request.get_json()
+    if item_data["store_id"] not in stores:
+        return {"message": "Store not found"}, 404
+
+    item_id = uuid.uuid4().hex
+    item = {**item_data, "id": item_id}
+    items[item_id] = item
+
+    return item
+
+
+@app.get("/store/<string:store_id>")
+def get_store(store_id):
+    try:
+        # Here you might also want to add the items in this store
+        # We'll do that later on in the course
+        return stores[store_id]
+    except KeyError:
+        return {"message": "Store not found"}, 404
+
+
+@app.get("/item/<string:item_id>")
+def get_item(item_id):
+    try:
+        return items[item_id]
+    except KeyError:
+        return {"message": "Item not found"}, 404
+
+
+@app.get("/item")
+def get_all_items():
+    return {"items": list(items.values())}
+
+
+if __name__ == '__main__':
+    # app.run(debug=True, host='0.0.0.0', port=8000) 
+    app.run(debug=True) 
