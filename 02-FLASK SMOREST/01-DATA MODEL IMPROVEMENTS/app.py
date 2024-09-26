a13e2148ae05b9ff7b4cc9055d8d457f7067ad9e
lak-sh-man <lakshman1122000@gmail.com>
2024-09-26 06:44:31 +0530

    commited

--- 02-FLASK SMOREST/01-DATA MODEL IMPROVEMENTS/app.py
+++ 02-FLASK SMOREST/01-DATA MODEL IMPROVEMENTS/app.py
@@ -7,12 +7,19 @@ from db import stores, items
 app = Flask(__name__)
 
 
-@app.get("/item/<string:item_id>")
-def get_item(item_id):
-    try:
-        return items[item_id]
-    except KeyError:
-        return {"message": "Item not found"}, 404
+@app.get("/store")
+def get_stores():
+    return {"stores": list(stores.values())}
+
+
+@app.post("/store")
+def create_store():
+    store_data = request.get_json()
+    store_id = uuid.uuid4().hex
+    store = {**store_data, "id": store_id}
+    stores[store_id] = store
+
+    return store
 
 
 @app.post("/item")
@@ -28,11 +35,6 @@ def create_item():
     return item
 
 
-@app.get("/item")
-def get_all_items():
-    return {"items": list(items.values())}
-
-
 @app.get("/store/<string:store_id>")
 def get_store(store_id):
     try:
@@ -43,16 +45,14 @@ def get_store(store_id):
         return {"message": "Store not found"}, 404
 
 
-@app.post("/store")
-def create_store():
-    store_data = request.get_json()
-    store_id = uuid.uuid4().hex
-    store = {**store_data, "id": store_id}
-    stores[store_id] = store
-
-    return store
+@app.get("/item/<string:item_id>")
+def get_item(item_id):
+    try:
+        return items[item_id]
+    except KeyError:
+        return {"message": "Item not found"}, 404
 
 
-@app.get("/store")
-def get_stores():
-    return {"stores": list(stores.values())}
+@app.get("/item")
+def get_all_items():
+    return {"items": list(items.values())}
