366e99055636f5bbb9c300fe6c09edef2d4b0a2c
lak-sh-man <lakshman1122000@gmail.com>
2024-09-26 13:08:30 +0530

    commited:\

--- 02-FLASK SMOREST/01-DATA MODEL IMPROVEMENTS/app.py
+++ 02-FLASK SMOREST/01-DATA MODEL IMPROVEMENTS/app.py
@@ -56,3 +56,8 @@ def get_item(item_id):
 @app.get("/item")
 def get_all_items():
     return {"items": list(items.values())}
+
+
+if __name__ == '__main__':
+    # app.run(debug=True, host='0.0.0.0', port=8000) 
+    app.run(debug=True) 
--- 02-FLASK SMOREST/02-API IMPROVEMENTS/app.py
+++ 02-FLASK SMOREST/02-API IMPROVEMENTS/app.py
@@ -8,12 +8,28 @@ from db import stores, items
 app = Flask(__name__)
 
 
-@app.get("/item/<string:item_id>")
-def get_item(item_id):
-    try:
-        return items[item_id]
-    except KeyError:
-        abort(404, message="Item not found.")
+@app.get("/store")
+def get_stores():
+    return {"stores": list(stores.values())}
+
+
+@app.post("/store")
+def create_store():
+    store_data = request.get_json()
+    if "name" not in store_data:
+        abort(
+            400,
+            message="Bad request. Ensure 'name' is included in the JSON payload.",
+        )
+    for store in stores.values():
+        if store_data["name"] == store["name"]:
+            abort(400, message=f"Store already exists.")
+
+    store_id = uuid.uuid4().hex
+    store = {**store_data, "id": store_id}
+    stores[store_id] = store
+
+    return store
 
 
 @app.post("/item")
@@ -45,11 +61,6 @@ def create_item():
     return item
 
 
-@app.get("/item")
-def get_all_items():
-    return {"items": list(items.values())}
-
-
 @app.get("/store/<string:store_id>")
 def get_store(store_id):
     try:
@@ -60,25 +71,19 @@ def get_store(store_id):
         abort(404, message="Store not found.")
 
 
-@app.post("/store")
-def create_store():
-    store_data = request.get_json()
-    if "name" not in store_data:
-        abort(
-            400,
-            message="Bad request. Ensure 'name' is included in the JSON payload.",
-        )
-    for store in stores.values():
-        if store_data["name"] == store["name"]:
-            abort(400, message=f"Store already exists.")
-
-    store_id = uuid.uuid4().hex
-    store = {**store_data, "id": store_id}
-    stores[store_id] = store
-
-    return store
+@app.get("/item/<string:item_id>")
+def get_item(item_id):
+    try:
+        return items[item_id]
+    except KeyError:
+        abort(404, message="Item not found.")
 
 
-@app.get("/store")
-def get_stores():
-    return {"stores": list(stores.values())}
+@app.get("/item")
+def get_all_items():
+    return {"items": list(items.values())}
+
+
+if __name__ == '__main__':
+    # app.run(debug=True, host='0.0.0.0', port=8000) 
+    app.run(debug=True) 
