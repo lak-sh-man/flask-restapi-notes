92193b4900bf2666b7c92f665f4ad02abe4b7a61
lak-sh-man <lakshman1122000@gmail.com>
2024-10-04 18:37:09 +0530

    commited

--- 03-Sql-Alchemy/01-Simple-Sql_alchemy-Model/app.py
+++ 03-Sql-Alchemy/01-Simple-Sql_alchemy-Model/app.py
@@ -19,3 +19,7 @@ api = Api(app)
 
 api.register_blueprint(ItemBlueprint)
 api.register_blueprint(StoreBlueprint)
+
+if __name__ == '__main__':
+    # app.run(debug=True, host='0.0.0.0', port=8000) 
+    app.run(debug=True) 
