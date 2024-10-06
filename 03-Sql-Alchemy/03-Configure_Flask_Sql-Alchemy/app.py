43bc86c4fe979df6b0848f65268743a86f495bcf
lak-sh-man <lakshman1122000@gmail.com>
2024-10-06 14:43:02 +0530

    commited

--- 03-Sql-Alchemy/03-Configure_Flask_Sql-Alchemy/app.py
+++ 03-Sql-Alchemy/03-Configure_Flask_Sql-Alchemy/app.py
@@ -1,25 +1,36 @@
 from flask import Flask
 from flask_smorest import Api
 
+from db import db
+
+import models
+
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
 
 
-app = Flask(__name__)
+def create_app(db_url=None):
+    app = Flask(__name__)
+    app.config["API_TITLE"] = "Stores REST API"
+    app.config["API_VERSION"] = "v1"
+    app.config["OPENAPI_VERSION"] = "3.0.3"
+    app.config["OPENAPI_URL_PREFIX"] = "/"
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+    app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or "sqlite:///data.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["PROPAGATE_EXCEPTIONS"] = True
+    db.init_app(app)
+    api = Api(app)
 
-app.config["PROPAGATE_EXCEPTIONS"] = True
-app.config["API_TITLE"] = "Stores REST API"
-app.config["API_VERSION"] = "v1"
-app.config["OPENAPI_VERSION"] = "3.0.3"
-app.config["OPENAPI_URL_PREFIX"] = "/"
-app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
-app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    with app.app_context():
+        db.create_all()
 
-api = Api(app)
+    api.register_blueprint(ItemBlueprint)
+    api.register_blueprint(StoreBlueprint)
 
-api.register_blueprint(ItemBlueprint)
-api.register_blueprint(StoreBlueprint)
+    return app
 
 if __name__ == '__main__':
-    # app.run(debug=True, host='0.0.0.0', port=8000) 
-    app.run(debug=True) 
+    app = create_app()  # Create the app instance
+    app.run(debug=True, host='0.0.0.0')  # Start the app
