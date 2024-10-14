c7df4ebedd68ab383298f255900fc1c41a33ac56
lak-sh-man <lakshman1122000@gmail.com>
2024-10-15 01:05:53 +0530

    commited

--- 03-Sql-Alchemy/00-Explaination/01-Without/01-Without-db.relationship.py
+++ 03-Sql-Alchemy/00-Explaination/01-Without/01-Without-db.relationship.py
@@ -2,37 +2,46 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-# This grabs our directory
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 
-# Connects our Flask App to our Database
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-db = SQLAlchemy()
 
-# Store Model
 class StoreModel(db.Model):
     __tablename__ = "stores"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
 
-# Item Model
+
 class ItemModel(db.Model):
     __tablename__ = "items"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
     price = db.Column(db.Float(precision=2), nullable=False)
-
-    # Foreign key to link with stores
     store_id = db.Column(db.Integer, db.ForeignKey("stores.id"), nullable=False)
 
-# Usage
-item = ItemModel.query.get(1)  # Get an item with ID 1
 
-# Without relationship, you need to manually query the store
-store = StoreModel.query.get(item.store_id)  # Find the store using the store_id
-print(f"Item: {item.name}, Store: {store.name}")  # Manually accessing the store
+    def __init__(self,name,price):
+        self.name = name
+        self.price = price
+
+
+with app.app_context():
+    my_item = ItemModel('Pen',50)
+    db.session.add(my_item)
+    db.session.commit()
+    
+    
+    item_1 = ItemModel.query.get(1)  # Get an item with ID 1
+
+
+    # Without relationship, you need to manually query the store
+    store = StoreModel.query.get(item_1.store_id)  # Find the store using the store_id
+    print(f"Item: {item_1.name}, Store: {store.name}")  # Manually accessing the store
--- 03-Sql-Alchemy/00-Explaination/02-With/02-With-db.relationship.py
+++ 03-Sql-Alchemy/00-Explaination/02-With/02-With-db.relationship.py
@@ -2,42 +2,40 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-# This grabs our directory
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 
-# Connects our Flask App to our Database
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-db = SQLAlchemy()
 
-# Store Model
 class StoreModel(db.Model):
     __tablename__ = "stores"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
-
-    # One-to-Many relationship: Store has many items
     items = db.relationship("ItemModel", back_populates="store")
 
-# Item Model
 class ItemModel(db.Model):
     __tablename__ = "items"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
     price = db.Column(db.Float(precision=2), nullable=False)
-
-    # Foreign key to link with stores
     store_id = db.Column(db.Integer, db.ForeignKey("stores.id"), nullable=False)
-
-    # Define the relationship with StoreModel
     store = db.relationship("StoreModel", back_populates="items")
 
-# Usage
-item = ItemModel.query.get(1)  # Get an item with ID 1
 
-# You can directly access the store details via the relationship
-print(f"Item: {item.name}, Store: {item.store.name}")  # Automatically gets the store name
+with app.app_context():
+    my_item = ItemModel('Pen',50)
+    db.session.add(my_item)
+    db.session.commit()
+    
+
+    item_1 = ItemModel.query.get(1)  # Get an item with ID 1
+
+    # You can directly access the store details via the relationship
+    print(f"Item: {item_1.name}, Store: {item_1.store.name}")  # Automatically gets the store name
