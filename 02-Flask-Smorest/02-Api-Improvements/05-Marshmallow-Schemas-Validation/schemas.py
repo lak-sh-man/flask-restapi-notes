bfacff1d4b61f2d2895480a19917e9326ff60e7b
lak-sh-man <lakshman1122000@gmail.com>
2024-10-14 12:27:04 +0530

    commited

--- 02-Flask-Smorest/02-Api-Improvements/05-Marshmallow-Schemas-Validation/schemas.py
+++ 02-Flask-Smorest/02-Api-Improvements/05-Marshmallow-Schemas-Validation/schemas.py
@@ -16,3 +16,11 @@ class ItemUpdateSchema(Schema):
 class StoreSchema(Schema):
     id = fields.Str(dump_only=True)
     name = fields.Str(required=True)
+
+
+"""Those parameters with "required=True" can only be sent in an API request""" 
+"""Any other more or less will throw an error"""
+"""Before using this schema, we might have put if condition like "name" should be mustly sent, but more that  
+   unwanted data can also be sent""" 
+"""This schema also helps to validate the values to check if they are in defined datatypes 
+   that are sent in defined keys"""
