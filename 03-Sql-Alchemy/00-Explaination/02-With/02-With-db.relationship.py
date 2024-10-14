fcc1c013284eb526bea211930a81a4b497890a4c
lak-sh-man <lakshman1122000@gmail.com>
2024-10-15 00:51:19 +0530

    commited

--- 03-Sql-Alchemy/00-Explaination/02-With/02-With-db.relationship.py
+++ 03-Sql-Alchemy/00-Explaination/02-With/02-With-db.relationship.py
@@ -1,4 +1,14 @@
+import os
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+
+# This grabs our directory
+basedir = os.path.abspath(os.path.dirname(__file__))
+app = Flask(__name__)
+
+# Connects our Flask App to our Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy()
 
