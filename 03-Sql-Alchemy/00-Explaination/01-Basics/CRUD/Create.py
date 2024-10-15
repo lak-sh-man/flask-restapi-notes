acbc366571dcd6868dc70a6c1b4931ebb20fce4e
lak-sh-man <lakshman1122000@gmail.com>
2024-10-15 18:21:37 +0530

    commited

--- 03-Sql-Alchemy/00-Explaination/01-Basics/CRUD/Create.py
+++ 03-Sql-Alchemy/00-Explaination/01-Basics/CRUD/Create.py
@@ -1,6 +1,15 @@
 # just like printing the class attributes or instance attributes doesn't fetch the data from table, it may only print the available data in local memory
 # there are special methods available to access data from table in db
 
+import sys
+import os
+
+# Get the parent directory (one level up from folder_1)
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
+# Add the parent directory to the system path
+sys.path.insert(0, parent_dir)
+
 from BasicModelApp import db, Puppy, app
 
 with app.app_context():
