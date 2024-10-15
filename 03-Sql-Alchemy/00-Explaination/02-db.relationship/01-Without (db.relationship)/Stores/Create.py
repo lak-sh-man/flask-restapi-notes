663bdca30e85fa6fdf93d40345e668270475550a
lak-sh-man <lakshman1122000@gmail.com>
2024-10-16 01:23:48 +0530

    commited

---
+++ 03-Sql-Alchemy/00-Explaination/02-db.relationship/01-Without (db.relationship)/Stores/Create.py
@@ -0,0 +1,22 @@
+import sys
+import os
+
+# Get the parent directory (one level up from folder_1)
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
+# Add the parent directory to the system path
+sys.path.insert(0, parent_dir)
+
+from db import db, StoreModel, app
+
+with app.app_context():
+    # Create 
+    store_1 = StoreModel(name="SS")
+    store_2 = StoreModel(name="Cheap & Best")
+    
+    db.session.add(store_1)
+    db.session.add(store_2)
+    
+    db.session.commit()
+
+    
