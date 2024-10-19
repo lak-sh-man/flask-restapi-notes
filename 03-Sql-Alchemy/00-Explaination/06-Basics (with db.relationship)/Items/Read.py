0357bd2d632e823cec9817025024de068ebaaa13
lak-sh-man <lakshman1122000@gmail.com>
2024-10-19 20:52:55 +0530

    commited

--- 03-Sql-Alchemy/00-Explaination/06-Basics (with db.relationship)/Items/Read.py
+++ 03-Sql-Alchemy/00-Explaination/06-Basics (with db.relationship)/Items/Read.py
@@ -7,15 +7,14 @@ parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 # Add the parent directory to the system path
 sys.path.insert(0, parent_dir)
 
-from BasicModelApp import db, StoreModel, app
+from BasicModelApp import db, ItemModel, app
 
 with app.app_context():
-    # Query a store
-    store = StoreModel.query.get(1)  # Get store with id=1
+    # Query an item
+    item = ItemModel.query.get(2)  # Get item with id=2
 
-    # Access the related items using the relationship
-    items = store.items
+    # Access the related store directly using the relationship
+    store = item.store
 
+    print(f"Item: {item.name}, Price: {item.price}")
     print(f"Store: {store.name}")
-    for item in items:
-        print(f"Item: {item.name}, Price: {item.price}")
