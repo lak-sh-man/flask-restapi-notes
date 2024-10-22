0f927c97ef1e5f5878c1e575bc598b4268c7f60e
lak-sh-man <lakshman1122000@gmail.com>
2024-10-22 15:06:43 +0530

    commited

---
+++ 04-Sql-Alchemy-Many-To-Many/02-Many-To-Many-Tags-Items/app.py
@@ -0,0 +1,38 @@
+import os
+from flask import Flask
+from flask_smorest import Api
+
+from db import db
+import models
+
+from resources.item import blp as ItemBlueprint
+from resources.store import blp as StoreBlueprint
+
+# This grabs our directory
+basedir = os.path.abspath(os.path.dirname(__file__))
+app = Flask(__name__)
+
+app.config["PROPAGATE_EXCEPTIONS"] = True
+app.config["API_TITLE"] = "Stores REST API"
+app.config["API_VERSION"] = "v1"
+app.config["OPENAPI_VERSION"] = "3.0.3"
+app.config["OPENAPI_URL_PREFIX"] = "/"
+app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["PROPAGATE_EXCEPTIONS"] = True
+db.init_app(app)
+api = Api(app)
+
+with app.app_context():
+    # this line only creates an empty puppy table, with column title using class attributes in it
+    # first db.create_all() should be done, then only object Instantiation should be done
+    db.create_all()  
+
+api.register_blueprint(ItemBlueprint)
+api.register_blueprint(StoreBlueprint)
+
+if __name__ == '__main__':
+    # app.run(debug=True, host='0.0.0.0', port=8000) 
+    app.run(debug=True) 
