1d172ee58331dcd07b0f7a2ce275acd562967d6c
lak-sh-man <lakshman1122000@gmail.com>
2024-10-22 17:57:50 +0530

    commited

--- 03-Sql-Alchemy/02-Cascades-Deleting-Store-Deletes-Items/schemas.py
+++ 03-Sql-Alchemy/02-Cascades-Deleting-Store-Deletes-Items/schemas.py
@@ -21,6 +21,12 @@ class ItemSchema(PlainItemSchema):
     store_id = fields.Str(required=True, load_only=True)
     store = fields.Nested(PlainStoreSchema(), dump_only=True)
 
+"""The schema (StoreSchema) is responsible for transforming these stores (which are Python objects) 
+   into a dictionary-like format (JSON or a Python dict)"""
 
 class StoreSchema(PlainStoreSchema):
-    items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
+    items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True) # If we hide this, items won't be returned
+
+
+"""Serialization: Converting Python objects (like SQLAlchemy models) into a format that can be returned to the client, such as JSON"""
+"""Deserialization: When data comes from the client, Marshmallow validates and converts the data into the expected format"""
