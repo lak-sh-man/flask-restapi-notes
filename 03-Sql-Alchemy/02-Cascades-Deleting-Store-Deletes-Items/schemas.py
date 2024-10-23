1021669d13b6a983c6f55c86720ef6c9d9dfdbcf
lak-sh-man <lakshman1122000@gmail.com>
2024-10-23 13:00:28 +0530

    commited

--- 03-Sql-Alchemy/02-Cascades-Deleting-Store-Deletes-Items/schemas.py
+++ 03-Sql-Alchemy/02-Cascades-Deleting-Store-Deletes-Items/schemas.py
@@ -24,21 +24,23 @@ class ItemSchema(PlainItemSchema):
     store_id = fields.Str(required=True, load_only=True)
     store = fields.Nested(PlainStoreSchema(), dump_only=True)
 
-"""The schema (StoreSchema) is responsible for transforming these stores (which are Python objects) 
-   into a dictionary-like format (JSON or a Python dict)"""
+"""1) The schema (StoreSchema) is responsible for transforming these stores (which are Python objects) 
+      into a dictionary-like format (JSON or a Python dict)
+   2) The related items and tags are fetched by SQLAlchemy when Marshmallow accesses them as part of the schema
+      This happens even though you only queried the StoreModel, because the relationships are dynamically loaded when needed"""
 
 class StoreSchema(PlainStoreSchema):
     items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True) # If we hide this, items won't be returned
 
 
-"""Serialization: Converting Python objects (like SQLAlchemy models) into a format that can be returned to the client, such as JSON"""
-"""Deserialization: When data comes from the client, Marshmallow validates and converts the data into the expected format"""
+"""1) Serialization: Converting Python objects (like SQLAlchemy models) into a format that can be returned to the client, such as JSON
+   2) Deserialization: When data comes from the client, Marshmallow validates and converts the data into the expected format"""
 
 
 
-"""Those parameters with "required=True" can only be sent in an API request""" 
-"""Any other more or less will throw an error"""
-"""Before using this schema, we might have put if condition like "name" should be mustly sent, but more that  
-   unwanted data can also be sent""" 
-"""This schema also helps to validate the values to check if they are in defined datatypes 
-   that are sent in defined keys"""
+"""1) Those parameters with "required=True" can only be sent in an API request
+      Any other more or less will throw an error
+   2) Before using this schema, we might have put if condition like "name" should be mustly sent, but more that  
+      unwanted data can also be sent
+   3) This schema also helps to validate the values to check if they are in defined datatypes 
+      that are sent in defined keys"""
