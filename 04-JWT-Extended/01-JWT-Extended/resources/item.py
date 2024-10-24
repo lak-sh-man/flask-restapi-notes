8ac03139a19143c0f7495267f2631cbb1a8de987
lak-sh-man <lakshman1122000@gmail.com>
2024-10-24 16:32:26 +0530

    commited

---
+++ 04-JWT-Extended/01-JWT-Extended/resources/item.py
@@ -0,0 +1,58 @@
+from flask.views import MethodView
+from flask_smorest import Blueprint, abort
+from sqlalchemy.exc import SQLAlchemyError
+
+from db import db
+from models import ItemModel
+from schemas import ItemSchema, ItemUpdateSchema
+
+blp = Blueprint("Items", __name__, description="Operations on items")
+
+
+@blp.route("/item/<string:item_id>")
+class Item(MethodView):
+    @blp.response(200, ItemSchema)
+    def get(self, item_id):
+        item = ItemModel.query.get_or_404(item_id)
+        return item
+
+    def delete(self, item_id):
+        item = ItemModel.query.get_or_404(item_id)
+        db.session.delete(item)
+        db.session.commit()
+        return {"message": "Item deleted."}
+
+    @blp.arguments(ItemUpdateSchema)
+    @blp.response(200, ItemSchema)
+    def put(self, item_data, item_id):
+        item = ItemModel.query.get_or_404(item_id)
+
+        item.price = item_data["price"]
+        item.name = item_data["name"]
+
+        db.session.add(item)
+        db.session.commit()
+
+        return item
+
+
+@blp.route("/item")
+class ItemList(MethodView):
+    @blp.response(200, ItemSchema(many=True))
+    def get(self):
+        return ItemModel.query.all()
+
+    @blp.arguments(ItemSchema)
+    @blp.response(201, ItemSchema)
+    def post(self, item_data): # here we mention the item_data as a parameter because, shema validates the json data 
+                               # then that validated json data can be passed automatically inside the post function by the schema behind the scene
+                               # that can be only achieved by passing it as a parameter (we didn't do this before)
+        item = ItemModel(**item_data)
+
+        try:
+            db.session.add(item)
+            db.session.commit()
+        except SQLAlchemyError:
+            abort(500, message="An error occurred while inserting the item.")
+
+        return item
